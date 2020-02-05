bdc6e31f6fb93df1bd9ff7842e77db76c353c9a4
백선기 <webseon@gmail.com>
2020-02-05 17:07:57 +0900

    - first commit

--- study/001_setServiceWorker/app.js
+++ study/001_setServiceWorker/app.js
@@ -1,18 +1,18 @@
 "use strict";
 import serviceWorkerCheck from "./modules/serviceWorkerCheck.js";
 
-export default function appStart() {
+function appStart() {
     serviceWorkerCheck('serviceWorker.js')
         .then(v => {
-            console.log('서비스 워커 등록 성공');
+            console.log('Service Worker Registration Success.');
             console.log(v);
-            document.body.innerHTML = '<h1>서비스 워커 등록 성공</h1>';
+            document.body.innerHTML = '<h1>Service Worker Registration Success.</h1>';
             for (const k in v) document.body.innerHTML += `<br>${k} - ${v[k]}`;
         })
         .catch(v => {
-            console.log('서비스 워커 등록 실패');
+            console.log('Service Worker Registration Fail.');
             console.log(v);
-            document.body.innerHTML = '<h1>서비스 워커 등록 실패</h1>';
+            document.body.innerHTML = '<h1>Service Worker Registration Fail</h1>';
         })
 }
 appStart();
--- study/001_setServiceWorker/serviceWorker.js
+++
@@ -1,2 +0,0 @@
-console.log('서비스워커파일실행');
-console.log(self);
