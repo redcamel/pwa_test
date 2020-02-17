be35bddc4285386bcc276290a5bcfc2d2db35dc4
백선기 <webseon@gmail.com>
2020-02-17 21:23:18 +0900

    - push test

--- study/004_pushTest/messaging-sw.js
+++ study/004_pushTest/messaging-sw.js
@@ -1,11 +1,17 @@
 console.log('서비스워커')
 self.addEventListener('push', function(event) {
     console.log('[Service Worker] Push Received.');
+    console.log(event)
     console.log(`[Service Worker] Push had this data: "${event.data.text()}"`);
 
-    const title = 'Push Codelab';
+    let data = event.data.json()
+    console.log(data)
+
+
+
+    const title = data['title'];
     const options = {
-        body: event.data.text(),
+        body: data['text'],
         icon: 'img/crate.png',
         badge: 'img/image.png'
     };
